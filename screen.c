e0ce0c6847399f427a04c8fc9e156c78d75f99cd
xxm1234567 <37363870+xxm1234567@users.noreply.github.com>
2018-04-24 08:08:34 +0000

    Update screen.c

--- screen.c
+++ screen.c
@@ -3,36 +3,34 @@
 #include "screen.h"
 #include <stdio.h>
 /***********************************************************************************
-	Punction definaition of clearScreen()
-	This function will use VT100 escape sequence "\ESC[2J" to erase the total
-	terminal screen
-	input arguement: none
-	return argument: none
+ * Function Name: clearScreen
+ * Discription  : This function will use VT100 escape sequence "\ESC[2J" to erase the total
+ *	          terminal screen
+ * Arguement    : none
+ * Return       : none
  ***********************************************************************************/
 void clearScreen(void){
 	printf("%c[2J",ESC);
 	fflush(stdout);
 }
 /***********************************************************************************
-	punction defination of gotoxy()
-	This function uses VT100 escape sequence "\ESC[row;colH" to set cursor
-	to the specific location of the terminal screen
-	input argument:
-		int row:	row number (1 is top)
-		int col:	col number (1 is left)
-	return argument: none
+ * Function Name: gotoxy
+ * Discription  : This function uses VT100 escape sequence "\ESC[row;colH" to set cursor
+ *	          to the specific location of the terminal screen
+ * Argument     : int row	row number (1 is top)
+ *		  int col	col number (1 is left)
+ * Return       : none
  ************************************************************************************/
 void gotoxy(int row, int col){
 	printf("%c[%d;%dH",ESC, row, col);
 	fflush(stdout);
 }
 /************************************************************************************
-	Punction defination of setColor()
-	This function uses VT100 escape sequence "\ESC[1;colorm" to use the different
-	color to label the intensity of sound of the terminal screen
-	input argument:
-		int color:	color number
-	return argument: none
+ * Function Name: setColor
+ * Discription  : This function uses VT100 escape sequence "\ESC[1;colorm" to use the different
+ *	          color to label the intensity of sound of the terminal screen
+ * Argument     : int color	color number
+ * Return       : none
  ************************************************************************************/
 
 
@@ -41,13 +39,12 @@ void setColor(int color){
 	fflush(stdout);
 }
 /*************************************************************************************
-	Punction defination of bar()
-	This function uses VT100 escape sequence "*" and "BAR" to illustrate the pitch
-	of sounds of the terminal screen
-	input argument:
-		int col:	col number(1 is left)
-		double dB:	dB number(sound intensity)
-	return argument: 	none
+ * Function Name: bar
+ * Discription  : This function uses VT100 escape sequence "*" and "BAR" to illustrate the pitch
+ *                of sounds of the terminal screen
+ * Argument     : int col	col number(1 is left)
+ *                double dB	dB number(sound intensity)
+ * Return       : none
  *************************************************************************************/
 void bar(int col, double dB){
 	int i;
